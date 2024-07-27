cff970619c129f29bc7794837b7b26382316e80d
Your Name <you@example.com>
2024-07-27 18:03:15 +0000

    v0.0.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Image from "next/image";
 import { MainSection } from "./sections/mainSection/ui";
-
+import classNames from "classnames";
+import styles from './page.module.css'
 export default function Home() {
   return (<>
 
+    <MainSection />
     <MainSection />
   </>);
 }
--- app/sections/mainSection/ui.tsx
+++ app/sections/mainSection/ui.tsx
@@ -2,10 +2,20 @@ import classNames from "classnames";
 import styles from './mainSection.module.css'
 import { Header } from "./components/header/ui";
 import { Menu } from "./components/manu/ui";
+import { Stamp } from "./components/Stamp/ui";
+import Image from "next/image";
 export function MainSection() {
   return (
     <section className={classNames(styles.item)}>
       <Header />
+      {/* < . ц> */}
+
+      <Stamp />
+
+      {/* Фон нейронов  */}
+      {/* <Image src='/neuronsLeft.svg' alt="neuronsLeft" width={1250} height={950} className={classNames(styles.neuronsLeft, styles.logsFont)} /> */}
+      {/* <Image src='/neuronsRight.svg' alt="neuronsLeft" width={1250} height={950} className={classNames(styles.neuronsRight, styles.logsFont)} /> */}
+
     </section>
   )
 }
